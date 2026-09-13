fad7151a690228785f9aefff28961fe6b48656bb
Kishanth B <kishanthb25@gmail.com>
2026-09-13 12:53:02 +0530

    Add POST /api/problems route adding a new problem entry

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+problems = [
+        {"name": "Two Sum", "difficulty": "Easy", "solved": True}, 
+        {"name": "Binary Tree Inorder Traversal", "difficulty": "Medium", "solved": True}
+    ]
 
 @app.route('/')
 def home():
     return "Welcome to my tracker!"
 
-@app.route('/api/problems')
-def get_problems():
-    problems = [
-        {"Name": "Two Sum", "Difficulty": "Easy", "Solved": True}, 
-        {"Name": "Binary Tree Inorder Traversal", "Difficulty": "Medium", "Solved": True}
-    ]
-    return jsonify(problems)
+@app.route('/api/problems', methods = ["GET", "POST"])
+def handle_problems():
+    if (request.method == "POST"):
+        new_problem = request.get_json()
+        problems.append(new_problem)
+        return jsonify(new_problem), 201
+    else:
+        return jsonify(problems)
 
 if __name__ == '__main__':
     app.run(debug = True)
