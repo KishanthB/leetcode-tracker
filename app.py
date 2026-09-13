bd865b9e7fc29ba433399473b5ea3b9b84ac0a97
Kishanth B <kishanthb25@gmail.com>
2026-09-13 10:51:52 +0530

    Add GET /api/problems route returning JSON problem list

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Welcome to my tracker!"
+
+@app.route('/api/problems')
+def get_problems():
+    problems = [
+        {"Name": "Two Sum", "Difficulty": "Easy", "Solved": True}, 
+        {"Name": "Binary Tree Inorder Traversal", "Difficulty": "Medium", "Solved": True}
+    ]
+    return jsonify(problems)
 
 if __name__ == '__main__':
     app.run(debug = True)
