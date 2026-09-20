635185876d127ed53d546ace412db88a0c1892e7
Kishanth B <kishanthb25@gmail.com>
2026-09-20 11:22:20 +0530

    Change problem_status to an enum type
    Replace Boolean so that problem can be 'Unsolved', 'Attempted', or 'Solved'

--- schema.sql
+++ schema.sql
@@ -5,3 +5,17 @@ CREATE TABLE problems(
     problem_difficulty TEXT NOT NULL,
     problem_status BOOLEAN DEFAULT FALSE NOT NULL
 );
+
+CREATE TYPE problem_status_enum AS ENUM ('Unsolved', 'Attempted', 'Solved');
+
+ALTER TABLE problems
+ALTER COLUMN problem_status 
+DROP DEFAULT;
+
+ALTER TABLE problems
+ALTER COLUMN problem_status TYPE problem_status_enum
+USING CASE WHEN problem_status THEN 'Solved' ELSE 'Unsolved' END::problem_status_enum;
+
+ALTER TABLE problems
+ALTER COLUMN problem_status
+SET DEFAULT 'Unsolved';
